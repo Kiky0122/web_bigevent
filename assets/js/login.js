4763e006b203bf16387dc19a2ac38dc603f33009
luchangqi <1924159899@qq.com>
2022-05-23 16:52:13 +0800

    完成了主页功能开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -17,7 +17,7 @@ $(function() {
     // 通过 form.verify() 函数自定义校验规则
     form.verify({
       // 自定义了一个叫做 pwd 校验规则
-      pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+      pwd: [/^[\S]{6,12}$/, '密码必须6到12位,且不能出现空格'],
       // 校验两次密码是否一致的规则
       repwd: function(value) {
         // 通过形参拿到的是确认密码框中的内容
@@ -50,7 +50,7 @@ $(function() {
       })
     })
   
-    // 监听登录表单的提交事件
+    // 账号登录事件监听登录表单的提交事件
     $('#form_login').submit(function(e) {
       // 阻止默认提交行为
       e.preventDefault()
@@ -67,7 +67,7 @@ $(function() {
           // 将登录成功得到的 token 字符串，保存到 localStorage 中
           localStorage.setItem('token', res.token)
           // 跳转到后台主页
-          // location.href = 'index.html'
+          location.href = 'index.html'
         }
       })
     })
